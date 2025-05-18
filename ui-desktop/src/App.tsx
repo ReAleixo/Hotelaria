71ab1071c24c23b77c19ffe19d5eef818476d029
Renan Aleixo <renan.aleixo7@gmail.com>
2025-05-18 14:23:03 -0300

    Inicio do front end do projeto

---
+++ ui-desktop/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold">Projeto Hotelaria</h1>
+    </div>
+  );
+}
+
+export default App;
